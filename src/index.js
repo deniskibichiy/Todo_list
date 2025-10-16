3a4a2605a39706364c903c43813c8328c3d6bf1f
Denis Kibichiy <deniskibichiy@outlook.com>
2025-10-16 21:41:14 +0300

    complete logic to handle task rendering under all items

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import "./style.css"
 import {createProject, projects} from "./projects";
 import { createItem, items } from "./todoItems";
 import{changePriority} from "./priority";
-import { mainRender, renderProjects } from "./render";
+import { mainRender,hideClass, renderAllItems, renderProjects } from "./render";
 
 
 const home = createProject("home", "Projects here related to home duties");
+const shopping = createProject("shopping", "going for some shopping");
 const school = createProject("school work", "Projects here related to school work");
 const assignment = createItem("Linear Algebra", "Complete systems of linear equations in linear algebra", "2025-10-19", "High");
 const cleanHouse = createItem("Clean", "Get my bedroom in order", "2025 - 10 -10", "High")
-const grocerShopping = createItem ("groceries", "purchase some groceries", "2025-12-34", "High")
-renderProjects(projects);
+const grocerShopping = createItem ("Groceries", "purchase some groceries", "2025-12-34", "High")
+document.querySelector("#projects").addEventListener("click", e => {
+
+            renderProjects(projects)
+})
+
 mainRender()
-
-
+renderAllItems(items)
 
--- src/render.js
+++ src/render.js
@@ -3,19 +3,40 @@
 function mainRender() {
     const container = document.querySelector("#container");
     const taskDiv = document.createElement("div");
-
-    container.textContent = "Let the rendering begin"
 }
 
 function renderProjects(arr) {
     const container = document.querySelector(".all-projects");
+    container.textContent = " ";
     arr.forEach(item => {
-        const listing = document.createElement("a");
         const btn = document.createElement("button");
         btn.textContent = `${item.name}`;
-        listing.appendChild(btn);
-        container.appendChild(listing);
+        container.appendChild(btn);
     });
 }
 
-export {mainRender, renderProjects}
+function renderAllItems (arr) {
+    const container = document.querySelector("#container");
+    arr.forEach(item => {
+        const itemDiv = document.createElement("div");
+        itemDiv.classList.add("itemDiv")
+        const itemHeading = document.createElement("h2");
+        itemHeading.textContent = `${item.title}`;
+        const description = document.createElement("p");
+        description.textContent = `${item.description}`;
+        const due = document.createElement("p");
+        due.textContent = `Due: ${item.dueDate}`
+        itemDiv.appendChild(itemHeading);
+        itemDiv.appendChild(description);
+        itemDiv.appendChild(due);
+        container.appendChild(itemDiv);
+    })
+}
+
+
+function hideClass () {
+    const container = document.querySelector(".all-projects");
+    container.classList.add("hidden")
+
+}
+export {mainRender, hideClass, renderProjects, renderAllItems}
